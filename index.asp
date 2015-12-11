98316c3bc5c13ed8858f2a297805785acacc7f30
Jian Huang <jhuangsoftware@gmail.com>
2015-12-11 16:45:39 -0500

    First commit

---
+++ index.asp
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="expires" content="-1"/>
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<meta name="copyright" content="2014, Web Solutions"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
+	<title>Standardfield To Image</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<style type="text/css">
+		body {
+			padding: 10px;
+		}
+	</style>
+	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/handlebars-v2.0.0.js"></script>
+	<script type="text/javascript" src="js/content-transfer.js"></script>
+	<script type="text/javascript" src="rqlconnector/Rqlconnector.js"></script>
+
+	<script id="template-action-dialog" type="text/x-handlebars-template" data-container="#action-dialog" data-action="replace">
+		<div class="modal hide fade" data-backdrop="static" tabindex="-1" role="dialog" aria-hidden="true">
+			<div class="modal-header">
+				<h4>Content Transfer</h4>
+			</div>
+			<div class="modal-body">
+				<form class="form-horizontal">
+					<div class="control-group">
+						<label class="control-label">Source</label>
+						<div class="controls source">
+							<input type="text" placeholder="Element Name" value="stf_document">
+						</div>
+					</div>
+					<div class="control-group">
+						<label class="control-label">Target</label>
+						<div class="controls target">
+							<input type="text" placeholder="Element Name" value="med_document">
+						</div>
+					</div>
+					<div class="control-group">
+						<div class="controls multi-threaded">
+							<label>
+								<input type="checkbox"> multi-threaded
+							</label>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<span class="btn btn-success transfer" data-dismiss="modal">Transfer</span>
+			</div>
+		</div>
+	</script>
+	
+	<script id="template-status" type="text/x-handlebars-template" data-container="#status" data-action="append">
+		<div class="{{guid}}">
+			<div class="alert">
+				Processing <strong>{{name}}</strong>
+				{{#if count}}
+				<div class="pull-right">{{count}} remaining</div>
+				{{/if}}
+			</div>
+		</div>
+	</script>
+	
+	<script id="template-status-remove" type="text/x-handlebars-template" data-container="#status" data-action="replace">
+	</script>
+	
+	<script type="text/javascript">
+		var LoginGuid = '<%= session("loginguid") %>';
+		var SessionKey = '<%= session("sessionkey") %>';
+		var ContentClassGuid = '<%= session("treeguid") %>';
+
+		$(document).ready(function() {
+			var RqlConnectorObj = new RqlConnector(LoginGuid, SessionKey);
+			var ContentTransferObj = new ContentTransfer(RqlConnectorObj, ContentClassGuid);
+		});
+	</script>
+</head>
+<body>
+	<div id="action-dialog"></div>
+	<div class="container" id="status">
+		<div class="alert">Loading</div>
+	</div>
+</body>
+</html>
---
+++ js/content-transfer.js
@@ -0,0 +1,135 @@
+function ContentTransfer(RqlConnectorObj, ContentClassGuid) {
+	var ThisClass = this;
+	this.RqlConnectorObj = RqlConnectorObj;
+	
+	this.TemplateActionDialog = '#template-action-dialog';
+	this.TemplateStatus = '#template-status';
+	this.TemplateStatusRemove = '#template-status-remove';
+	
+	this.UpdateArea(this.TemplateActionDialog);
+	
+	var ActionDialogContainer = $(this.TemplateActionDialog).attr('data-container');
+	$ActionDialogModal = $(ActionDialogContainer).find('.modal');
+	$ActionDialogModal.modal('show');
+	$(ActionDialogContainer).find('.modal');
+	
+	$(ActionDialogContainer).on('click', '.transfer', function(){
+		ThisClass.GetAllPageInstancesArray(ContentClassGuid, function(PageInstancesArray){
+			var SourceElementName = $(ActionDialogContainer).find('.source input').val();
+			var TargetElementName = $(ActionDialogContainer).find('.target input').val();
+			var IsMultiThreaded = $(ActionDialogContainer).find('.multi-threaded input').prop('checked');
+			
+			ThisClass.UpdateArea(ThisClass.TemplateStatusRemove);
+			
+			if(IsMultiThreaded) {
+				// threaded method
+				$.each(PageInstancesArray, function() {
+					ThisClass.ReferencePage(this, SourceElementName, TargetElementName);
+				});
+			} else {
+				// single threaded
+				ThisClass.ReferenceArray(PageInstancesArray, SourceElementName, TargetElementName);
+			}
+		});
+	});
+}
+
+ContentTransfer.prototype.GetAllPageInstancesArray = function(ContentClassGuid, CallbackFunc) {
+	var ThisClass = this;
+
+	var RqlXml = '<PAGE action="xsearch" pagesize="-1" maxhits="-1"><SEARCHITEMS><SEARCHITEM key="contentclassguid" value="' + ContentClassGuid + '" operator="eq"></SEARCHITEM></SEARCHITEMS></PAGE>';
+	this.RqlConnectorObj.SendRql(RqlXml, false, function(data){
+		var PageInstancesArray = [];
+
+		$(data).find('PAGE').each( function(index) {
+			var PageObj = {
+				guid: $(this).attr('guid'),
+				name: $(this).attr('headline')
+			};
+			PageInstancesArray.push(PageObj);
+		});
+
+		CallbackFunc(PageInstancesArray);
+	});
+}
+
+ContentTransfer.prototype.ReferenceArray = function(PageInstancesArray, SourceElementName, TargetElementName) {
+	var ThisClass = this;
+	
+	var PageObj = PageInstancesArray.shift();
+	if(PageObj)
+	{
+		var RqlXml = '<PAGE guid="' + PageObj.guid + '"><ELEMENTS action="load"/></PAGE>';
+		PageObj.count = PageInstancesArray.length;
+		
+		ThisClass.UpdateArea(ThisClass.TemplateStatus, undefined, PageObj);
+
+		ThisClass.RqlConnectorObj.SendRql(RqlXml, false, function(data){
+			var SourceElementValue = $(data).find('ELEMENT[eltname="' + SourceElementName + '"]').attr('value');
+			var TargetElementGuid = $(data).find('ELEMENT[eltname="' + TargetElementName + '"]').attr('guid');
+			
+			RqlXml = '<ELT action="save" reddotcacheguid="" guid="' + TargetElementGuid + '" value="' + SourceElementValue + '"></ELT>'
+			
+			ThisClass.RqlConnectorObj.SendRql(RqlXml, false, function(data){
+				ThisClass.UpdateArea(ThisClass.TemplateStatusRemove, ' .' + PageObj.guid);
+				
+				ThisClass.ReferenceArray(PageInstancesArray, SourceElementName, TargetElementName);
+			});
+		});
+	}
+}
+
+ContentTransfer.prototype.ReferencePage = function(PageObj, SourceElementName, TargetElementName, CallbackFunc) {
+	var ThisClass = this;
+	
+	if(PageObj)
+	{
+		var RqlXml = '<PAGE guid="' + PageObj.guid + '"><ELEMENTS action="load"/></PAGE>';
+		
+		ThisClass.UpdateArea(ThisClass.TemplateStatus, undefined, PageObj);
+
+		ThisClass.RqlConnectorObj.SendRql(RqlXml, false, function(data){
+			var SourceElementValue = $(data).find('ELEMENT[eltname="' + SourceElementName + '"]').attr('value');
+			var TargetElementGuid = $(data).find('ELEMENT[eltname="' + TargetElementName + '"]').attr('guid');
+			
+			RqlXml = '<ELT action="save" reddotcacheguid="" guid="' + TargetElementGuid + '" value="' + SourceElementValue + '"></ELT>'
+			
+			ThisClass.RqlConnectorObj.SendRql(RqlXml, false, function(data){
+				ThisClass.UpdateArea(ThisClass.TemplateStatusRemove, ' .' + PageObj.guid);
+				
+				if(CallbackFunc){
+					CallbackFunc();
+				}
+			});
+		});
+	}
+}
+
+ContentTransfer.prototype.UpdateArea = function(TemplateId, DataContainerAdditional, Data){
+	var ContainerId = $(TemplateId).attr('data-container');
+	if(DataContainerAdditional){
+		ContainerId += DataContainerAdditional;
+	}
+	var TemplateAction = $(TemplateId).attr('data-action');
+	var Template = Handlebars.compile($(TemplateId).html());
+	var TemplateData = Template(Data);
+
+	if((TemplateAction == 'append') || (TemplateAction == 'replace'))
+	{
+		if (TemplateAction == 'replace') {
+			$(ContainerId).empty();
+		}
+
+		$(ContainerId).append(TemplateData);
+	}
+
+	if(TemplateAction == 'prepend')
+	{
+		$(ContainerId).prepend(TemplateData);
+	}
+
+	if(TemplateAction == 'after')
+	{
+		$(ContainerId).after(TemplateData);
+	}
+}
